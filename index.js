a5282e45042212a1d286fcc9715a455929107dc0
Anthony Carbajal <acarbajal721@live.com>
2019-06-19 00:01:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ function happyHolidays(){
   return "Happy Holidays!";
 }
 
-function happyHolidayTo(name){
+function happyHolidaysTo(name){
   return `Happy Holidays, ${name}!`;
+}
+function happyHolidayTo(holiday name){
+  return `Happy ${holiday}, ${name}!`;
 }
 function holidayCountdown(holiday, days){
   return `It's ${days} days until ${holiday}!`;
